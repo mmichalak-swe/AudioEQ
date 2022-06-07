ecb8e792a4ee00ebd06a8f8a6c72d7d82877c4a6
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-07 18:07:53 -0400

    added gridlines, pre rendered grid into background

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -230,8 +230,11 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
     using namespace juce;
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (Colours::black);
+    
+    g.drawImage(background, getLocalBounds().toFloat());
 
-    auto responseArea = getLocalBounds();
+//    auto responseArea = getLocalBounds();
+    auto responseArea = getAnalysisArea();
 
     auto w = responseArea.getWidth();
     
@@ -291,13 +294,84 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
     }
     
     g.setColour(Colours::orange);
-    g.drawRoundedRectangle(responseArea.toFloat(), 4.f, 1.f);
+    g.drawRoundedRectangle(getRenderArea().toFloat(), 4.f, 1.f);
     
     g.setColour(Colours::white);
     g.strokePath(responseCurve, PathStrokeType(2.f));
     
 }
 
+void ResponseCurveComponent::resized()
+{
+    using namespace juce;
+    background = Image(Image::PixelFormat::RGB, getWidth(), getHeight(), true);
+    
+    Graphics g(background);
+    
+    Array<float> freqs
+    {
+        20, 30, 40, 50, 100,
+        200, 300, 400, 500, 1000,
+        2000, 3000, 4000, 5000, 10000,
+        20000
+    };
+    
+    auto renderArea = getAnalysisArea();
+    auto left = renderArea.getX();
+    auto right = renderArea.getRight();
+    auto top = renderArea.getY();
+    auto bottom = renderArea.getBottom();
+    auto width = renderArea.getWidth();
+    
+    Array<float> xs;
+    for (auto f: freqs)
+    {
+        auto normX = mapFromLog10(f, 20.f, 20000.f);
+        xs.add(left + width * normX);
+    }
+    
+    g.setColour(Colours::dimgrey);
+    for (auto x : xs)
+    {
+        g.drawVerticalLine(x, top, bottom);
+    }
+
+    Array<float> gain
+    {
+        -24, -12, 0, 12, 24
+    };
+    
+    for (auto gDb: gain)
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+
+        g.setColour(gDb == 0.f ? Colour(4u, 106u, 56u) : Colours::darkgrey);
+        g.drawHorizontalLine(y, left, right);
+    }
+
+}
+
+juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
+{
+    auto bounds = getLocalBounds();
+    
+//    bounds.reduce(10, //JUCE_LIVE_CONSTANT(5),
+//                  8 //JUCE_LIVE_CONSTANT(5));
+//                  );
+    bounds.removeFromTop(12);
+    bounds.removeFromBottom(2);
+    bounds.removeFromLeft(20);
+    bounds.removeFromRight(20);
+    return bounds;
+}
+
+juce::Rectangle<int> ResponseCurveComponent::getAnalysisArea()
+{
+    auto bounds = getRenderArea();
+    bounds.removeFromTop(4);
+    bounds.removeFromBottom(4);
+    return bounds;
+}
 //==============================================================================
 AudioEQAudioProcessorEditor::AudioEQAudioProcessorEditor (AudioEQAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p),
