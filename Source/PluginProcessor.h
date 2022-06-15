3afb634a4bb01406c805df7e497ecf1f94418120
Mitchell <38023623+mmichalak-swe@users.noreply.github.com>
2022-06-15 18:33:02 -0400

    add bypass button and ability to bypass bands response curve adheres to bypass state

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -155,6 +155,10 @@ struct ChainSettings
     float peakFreq {0}, peakGainInDecibels {0}, peakQuality {1.f};
     float lowCutFreq {0}, highCutFreq {0};
     Slope lowCutSlope {Slope::Slope_12}, highCutSlope {Slope::Slope_12};
+    
+    bool lowCutBypassed = false;
+    bool peakBypassed = false;
+    bool highCutBypassed = false;
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
